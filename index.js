de205a653dd1ef3d3b426236ed094787e8489975
Brayden Cheek <bwc1024@hotmail.com>
2023-03-31 04:31:19 -0400

    Write slack message for github issue webhook

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-exports.handler = async (event) => {
-  console.log("Hello, world!");
+const https = require('https');
+
+exports.handler = async (event, context) => {
+  const payload = JSON.parse(event.body);
+  const slackUrl = process.env.SLACK_URL;
+  
+  const message = {
+    text: `Issue Created: ${payload.issue.html_url}`
+  };
+  
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  
+  const req = https.request(slackUrl, options, (res) => {
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`Response: ${chunk}`);
+    });
+  });
+  
+  req.on('error', (e) => {
+    console.error(`Error: ${e.message}`);
+  });
+  
+  req.write(JSON.stringify(message));
+  req.end();
 };
